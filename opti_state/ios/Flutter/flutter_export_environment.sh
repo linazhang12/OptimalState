9ad24826822f528f3e9d436599ccc8e0fc9d87e8
linazhang12 <linazhang13579@gmail.com>
2020-02-15 23:10:03 -0600

    workssksskks

---
+++ opti_state/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/linazhang/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/linazhang/Desktop/OptimalState/opti_state"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/linazhang/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
